71680a4410dcdef731d79f7e64ad5a720b83c3af
Shadi Rada <shakkyr@gmail.com>
2022-04-10 23:21:06 +0300

    post

--- client/src/components/posts/post/Post.js
+++ client/src/components/posts/post/Post.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import DeleteIcon from '@material-ui/icons/Delete'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+
 import makeStyles from './styles'
 
-const Post = () => {
+const Post = ({post}) => {
     const classes = makeStyles()
   return (
-    <h1>Post</h1>
+    <Card className={classes.card} >
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+    </Card>
   )
 }
 
