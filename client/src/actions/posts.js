65578115df9e6a3079b61f1af1517857826c63b9
Shadi Rada <shakkyr@gmail.com>
2022-04-11 23:49:25 +0300

    post update

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -13,21 +13,21 @@ export const getPosts = () => async (dispatch) => {
 }
 
 export const createPost = (post) => async (dispatch) => {
-    try {
-        const { data} = await api.createPost(post)
+  try {
+    const { data } = await api.createPost(post);
 
-        dispatch({type: 'CREATE', payload: data})
-    } catch (error) {
-        console.log(error);
-    }
-}
+    dispatch({ type: "CREATE", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const updatePost = (id, post) => async (dispatch) => {
     try {
-        const {data} = await api.updatePost(id, post)
-
-        dispatch({type: "UPDATE", payload: data})
-    } catch (err) {
-        console.log(err.message);
+      const { data } = await api.updatePost(id, post);
+  
+      dispatch({ type: "UPDATE", payload: data });
+    } catch (error) {
+      console.log(error.message);
     }
-}
+  };
