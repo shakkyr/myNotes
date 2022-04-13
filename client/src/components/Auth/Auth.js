c67784c0608df533488456ea0a05c8dd1ad1c178
Shadi Rada <shakkyr@gmail.com>
2022-04-13 04:41:20 +0300

    auth inputs

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOpenOutlined'
+import makeStyles from "./Styles";
+import Input from './Input'
 
 const Auth = () => {
-    const state = null;
+    const classes = makeStyles();
+
+    const isSignUp = false;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const handleChange = () => {
+
+    }
+
   return (
-    <div>Auth</div>
+    <Container component="main" maxWidth="xs">
+        <Paper className={classes.paper} elevation={3}>
+            <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography variant="h5">
+                {isSignUp ? 'Sign Up' : 'Sign In'}
+            </Typography>
+            <form className={classes.form} onSubmit={handleSubmit}> 
+                <Grid container spacing={2}>
+                    {
+                        isSignUp && (
+                            <>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} half/>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} half/>
+                            </>
+                        )
+                    }
+                </Grid>
+            </form>
+        </Paper>
+    </Container>
   )
 }
 
